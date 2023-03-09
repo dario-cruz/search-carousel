baac4ff944eda57ca9d350a3c0a6c9d7f627d6a4
Dario Cruz <rio2012@gmail.com>
2023-03-09 11:15:57 -0500

    Add changes to webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import { testImages } from "./getImages"
 
 // Create the main page frame.
 const navBar = document.createElement('nav')
@@ -7,3 +7,5 @@ const sidePanel = document.createElement('div')
 
 const mainPanel = document.createElement('div')
 
+
+testImages(amazon)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const webpack = require('webpack') // access for built in plugins.
 const path = require('path')
+const { experiments } = require('webpack')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
-}
+    },
+    experiments: {
+        topLevelAwait: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use:['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
