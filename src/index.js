107bfc58cc7924365523373a2ce340698fd98e53
Dario Cruz <rio2012@gmail.com>
2023-03-10 07:19:42 -0500

    Add config changes and modules for app.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import { testImages } from "./getImages"
+import './index.css'
+
+// Body element to attach the rest of the dom elements to.
+const docBody = document.body
+
 
 // Create the main page frame.
+// Container for all content
+const pageFrame = document.createElement('div')
+
+// Navigation bar.
 const navBar = document.createElement('nav')
 
+// Side panel to scroll through images.
 const sidePanel = document.createElement('div')
 
+// Main panel to display images.
 const mainPanel = document.createElement('div')
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const webpack = require('webpack') // access for built in plugins.
 const path = require('path')
 const { experiments } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Search Carosiel'
+        }),
+    ],
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     experiments: {
         topLevelAwait: true
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port:8080,
     },
     module: {
         rules: [
@@ -17,6 +32,14 @@ module.exports = {
                 test: /\.css$/i,
                 use:['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+              },
         ],
     },
 };
