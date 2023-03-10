8d3413c2a4d561e3bfdbeb6286e3a80a35973604
Dario Cruz <rio2012@gmail.com>
2023-03-10 10:03:33 -0500

    Add code for pageFrame and appending to html body.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import { testImages } from "./getImages"
+import attributeHelper from "@dariocru/attribute-helper"
 import './index.css'
 
 // Body element to attach the rest of the dom elements to.
 const docBody = document.body
 
-
 // Create the main page frame.
 // Container for all content
 const pageFrame = document.createElement('div')
+attributeHelper(pageFrame, {'class':'pageFrame'})
+docBody.append(pageFrame)
 
 // Navigation bar.
 const navBar = document.createElement('nav')
+attributeHelper(navBar, {'class':'navBar'})
 
 // Side panel to scroll through images.
 const sidePanel = document.createElement('div')
+attributeHelper(sidePanel, {'class':'sidePanel'})
 
 // Main panel to display images.
 const mainPanel = document.createElement('div')
+attributeHelper(mainPanel, {'class':'mainPanel'})
 
+// Append all the things. 
+pageFrame.append(navBar, sidePanel, mainPanel)
 
 testImages(amazon)
