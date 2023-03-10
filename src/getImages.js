c41e679ebb3548cfc28126c9eea5a0b0caad87a0
Dario Cruz <rio2012@gmail.com>
2023-03-10 10:08:17 -0500

    Add comments to code in getImages.js and index.js

--- src/getImages.js
+++ src/getImages.js
@@ -1,9 +1,14 @@
 import google from 'googlethis'
 
+// Term that the user will search for.
+let term = ''
+// Code for search of google images. 
 const  getSearchImages = await google.image(`${term}`, { safe: true })
 
-function testImages(term) {
-    console.log(getSearchImages)
+// Changes the term to the user input and runs the getSearchImages func.
+function testImages(userInput) {
+    term = userInput
+    console.log(getSearchImages())
 }
 
 export{testImages}
