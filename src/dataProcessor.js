272f4b5404782540f2d76d8b3ac5d0ed4187ae68
Cruz <dariocru@amazon.com>
2023-03-17 08:06:11 -0400

    Removed commented codee from searchApi.js, added dataProcessor.js module for processing object data from promise.

---
+++ src/dataProcessor.js
@@ -0,0 +1,2 @@
+import { searchPexels } from "./searchApi";
+
--- src/searchApi.js
+++ src/searchApi.js
@@ -1,10 +1,3 @@
-import { createClient } from "pexels";
-
-
-// const client = createClient('VhTnt4oP6hpy8kAUcQUkFSUNm4yiIqyZ2qNbY3Gzo8EwgU872H4PbcMX')
-
-// client.photos.search({searchQuery, per_page: 10}).then(photos => {...})
-
 async function searchPexels(input) {
     const baseUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=10`
     const apiKey = 'VhTnt4oP6hpy8kAUcQUkFSUNm4yiIqyZ2qNbY3Gzo8EwgU872H4PbcMX'
